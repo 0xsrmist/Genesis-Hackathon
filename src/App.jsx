e8d8e09483c9d62c3f1ff10f9d481f5bed5b030d
Abhishek Yadav <abhishekaug2002@gmail.com>
2023-11-21 20:41:35 +0530

    project initiated

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+
+
+function App() {
+
+  return (
+    <>
+      <div className="flex flex-col gap-2 p-2">
+        {/* Import functions here */}
+      </div>
+    </>
+  )
+}
+
+export default App
