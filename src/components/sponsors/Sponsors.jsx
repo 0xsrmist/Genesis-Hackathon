13cd6adfe6a69076c65b7c79cff0eee111080236
nihal singh <nihalsingh.net@gmail.com>
2023-11-22 10:19:29 +0530

    message

--- src/components/sponsors/Sponsors.jsx
+++ src/components/sponsors/Sponsors.jsx
@@ -1,12 +1,21 @@
 // Sponsor.js
 import React from 'react';
 
-const Sponsor = ({ name, logo, description }) => (
-  <div className="sponsor">
-    <img src={logo} alt={`${name} logo`} />
-    <h3>{name}</h3>
-    <p>{description}</p>
-  </div>
-);
+const Sponsor = ({ name, logo, description }) => {
+  // Render a div with a class name 'sponsor'
+  return (
+    <div className="sponsor">
+      {/* Render an image with the source set to the logo prop */}
+      <img src={logo} alt={`${name} logo`} />
+      
+      {/* Render an h3 element with the name prop */}
+      <h3>{name}</h3>
+      
+      {/* Render a paragraph with the description prop */}
+      <p>{description}</p>
+    </div>
+  );
+};
 
+// Export the Sponsor component to make it available for import in other files
 export default Sponsor;
