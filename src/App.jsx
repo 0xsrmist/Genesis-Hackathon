39416a324167bd45ed3bf677b2efd54d437ffd31
Samarth Mishra <122509056+FUTHARVSTUD@users.noreply.github.com>
2023-11-23 00:30:16 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,11 @@
+import React from "react";
+import navbar from "./components/navbar/navbar.jsx";
+
 function App() {
   return (
     <>
       <div className="flex flex-col gap-2 p-2">
+        <navbar />
       </div>
     </>
   );
