77280e8bd49c66803301f964f5167f226f1125a4
Samarth Mishra <122509056+FUTHARVSTUD@users.noreply.github.com>
2023-11-23 00:54:20 +0530

    resolved merging issues

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,10 @@
-import React from "react";
-import Navbar from "./components/navbar/Navbar.jsx";
-
 function App() {
   return (
     <>
       <div className="flex flex-col gap-2 p-2">
-        <Navbar />
       </div>
     </>
   );
 }
+
 export default App;
