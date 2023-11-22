7eb939042a09271f7c88be4df8554338adcb160a
Dhruv Sovasaria <dhruvsovasaria34@gmail.com>
2023-11-22 18:37:37 +0530

    hero component added

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,15 @@
-
+import React from "react";
+import Hero from "./components/hero/Hero.jsx";
 
 function App() {
-
   return (
     <>
       <div className="flex flex-col gap-2 p-2">
         {/* Import functions here */}
+        <Hero />
       </div>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
