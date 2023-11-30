e7b2b8dcef9702957f5ce0c90432e94b7efa680c
Abhishek Yadav <abhishekaug2002@gmail.com>
2023-11-30 13:21:26 +0530

    package changes

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,10 @@
-import GreyBox from "./components/faqs/Faqs.jsx";
+import Faqs from "./components/faqs/Faqs.jsx";
 
 function App() {
   return (
     <>
       <div className="flex flex-col gap-2 p-2">
-        <GreyBox />
+        <Faqs />
       </div>
     </>
   );
